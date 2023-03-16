1b11b4e73af7d29054c4dd373a5c5fcba0983e17
czerewacz@gmail.com <czerewacz@gmail.com>
2023-03-16 15:51:13 +0100

    Add delete item animation

--- app/src/main/java/com/example/to_do_list/ui/screens/list/ListContent.kt
+++ app/src/main/java/com/example/to_do_list/ui/screens/list/ListContent.kt
@@ -1,6 +1,10 @@
 package com.example.to_do_list.ui.screens.list
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -9,8 +13,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -20,6 +23,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.to_do_list.R
 import com.example.to_do_list.data.models.Priority
 import com.example.to_do_list.data.models.ToDoTask
@@ -27,6 +31,8 @@ import com.example.to_do_list.ui.theme.*
 import com.example.to_do_list.util.Action
 import com.example.to_do_list.util.RequestState
 import com.example.to_do_list.util.SearchAppBarState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun ListContent(
@@ -114,25 +120,51 @@ fun DisplayTasks(
             val isDismissed = dismissState.isDismissed(DismissDirection.EndToStart)
 
             if (isDismissed && dismissDirection == DismissDirection.EndToStart) {
-                onSwipeToDelete(Action.DELETE, task)
+                LaunchedEffect(key1 = true){
+                    delay(300)
+                    onSwipeToDelete(Action.DELETE, task)
+                }
             }
 
             val degrees by animateFloatAsState(
-                if (dismissState.targetValue == DismissValue.Default) 0f else -45f
+                if (dismissState.targetValue == DismissValue.Default)
+                    0f
+                else
+                    -45f
             )
 
-            SwipeToDismiss(
-                state = dismissState,
-                directions = setOf(DismissDirection.EndToStart),
-                dismissThresholds = { FractionalThreshold(fraction = 0.2f) },
-                background = { RedBackground(degrees = degrees) },
-                dismissContent = {
-                    TaskItem(
-                        toDoTask = task,
-                        navigateToTaskScreen = navigateToTaskScreen
+            var itemAppeared by remember { mutableStateOf(false) }
+
+            LaunchedEffect(key1 = true) {
+                itemAppeared = true
+            }
+
+            AnimatedVisibility(
+                visible = itemAppeared && !isDismissed,
+                enter = expandVertically(
+                    animationSpec = tween(
+                        durationMillis = 300
                     )
-                }
-            )
+                ),
+                exit = shrinkVertically(
+                    animationSpec = tween(
+                        durationMillis = 300
+                    )
+                )
+            ) {
+                SwipeToDismiss(
+                    state = dismissState,
+                    directions = setOf(DismissDirection.EndToStart),
+                    dismissThresholds = { FractionalThreshold(fraction = 0.2f) },
+                    background = { RedBackground(degrees = degrees) },
+                    dismissContent = {
+                        TaskItem(
+                            toDoTask = task,
+                            navigateToTaskScreen = navigateToTaskScreen
+                        )
+                    }
+                )
+            }
         }
     }
 }
@@ -224,4 +256,12 @@ fun TaskItemPreview() {
             priority = Priority.MEDIUM
         ),
         navigateToTaskScreen = {})
+}
+
+@Composable
+@Preview
+private fun RedBackgroundPreview() {
+    Box(modifier = Modifier.height(100.dp)) {
+        RedBackground(degrees = 0f)
+    }
 }
